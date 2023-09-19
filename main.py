51847e99d1518d001b2a325ce44fe7876eb75284
Cherrine <66070031@kmitl.ac.th>
2023-09-19 16:34:43 +0700

    change bot description

--- main.py
+++ main.py
@@ -14,17 +14,17 @@ async def on_ready():
     except Exception as e:
         print(e)
 
-#bot says hello to the user
-@client.tree.command(name="hello")
+#bot says hello to the user / for test purposes
+@client.tree.command(name="hello", description="Bot says hi to you")
 async def hello(interaction: discord.Interaction):
     await interaction.response.send_message(f"Hey {interaction.user.mention}! This is GG study bot!",\
         ephemeral=True)
 
-#say command(kinda like tts)
-@client.tree.command(name="say")
-@app_commands.describe(thingtosay="What should i say?")
+#say command(kinda like tts) / for test purposes
+@client.tree.command(name="say", description="What should I say?")
 async def say(interaction: discord.Interaction, thingtosay: str):
     await interaction.response.send_message(f"{interaction.user.name} said: `{thingtosay}`")
+
 
 client.run(config.TOKEN)
 
