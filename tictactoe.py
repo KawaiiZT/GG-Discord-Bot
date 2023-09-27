48ee5b7a5f904b30b7e6bb3fc5ec3facf4780bb6
66070067 <141384976+66070067@users.noreply.github.com>
2023-09-27 23:06:39 +0700

    Update tictactoe.py
    import built-in function + make winningConditions list

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,24 @@
 ##tictactoe
+import discord
+from discord.ext import commands
+import random
+
+client = commands.Bot(command_prefix="!")
+
+player1 = ""
+player2 = ""
+turn = ""
+gameOver = True
+
+board = []
+
+winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
