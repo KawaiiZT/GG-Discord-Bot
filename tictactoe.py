de70841d0400b06b29ee4e0a3c88dcf32195c063
66070067 <141384976+66070067@users.noreply.github.com>
2023-09-28 13:07:26 +0700

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,4 @@
-##tictactoe
+##tic tac toe kmitl project bot
 import discord
 from discord.ext import commands
 import random
@@ -22,3 +22,23 @@ winningConditions = [
     [0, 4, 8],
     [2, 4, 6]
 ]
+
+@client.command()
+async def tictactoe(ctx, p1: discord.Member, p2: discord.Member):
+    global count
+    global player1
+    global player2
+    global turn
+    global gameOver
+
+    if gameOver:
+        global board
+        board = [":white_large_square:", ":white_large_square:", ":white_large_square:",
+                 ":white_large_square:", ":white_large_square:", ":white_large_square:",
+                 ":white_large_square:", ":white_large_square:", ":white_large_square:"]
+        turn = ""
+        gameOver = False
+        count = 0
+
+        player1 = p1
+        player2 = p2
