5e41b180154dafabd8f26324e0d7373b5255594d
KawaiiZT <95923980+KawaiiZT@users.noreply.github.com>
2023-10-02 22:08:57 +0700

    update some website file

--- InviteDiscordwebsite/oauth.py
+++ InviteDiscordwebsite/oauth.py
@@ -1,10 +1,10 @@
 import requests
 
 class Oauth:
-    client_id = ""
-    client_secret = ""
-    redirect_url = ""
-    scope = ""
-    discord_login_url = ""
-    discord_token_url = ""
-    discord_api_url = ""
+    client_id = "1153559337751547996"
+    client_secret = "JXXNTX1e1N7zl5usO5PD5DPNPxvUNXX0"
+    redirect_url = "http://127.0.0.1:5000/login"
+    scope = "identify%20email%20guilds"
+    discord_login_url = "https://discord.com/api/oauth2/authorize?client_id=1153559337751547996&redirect_uri=http%3A%2F%2F127.0.0.1%3A5000%2Flogin&response_type=code&scope=identify%20email%20guilds"
+    discord_token_url = "https://discord.com/api/oauth2/token"
+    discord_api_url = "https://discord.com/api"
--- InviteDiscordwebsite/website.py
+++ InviteDiscordwebsite/website.py
@@ -5,11 +5,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html",discord_url= "Oauth.discord_login_url")
+    return render_template("index.html",discord_url= Oauth.discord_login_url)
 
 @app.route("/login")
 def login():
     code = request.args.get("code")
+
+    at = Oauth.get_access_token
     return "Success"
 if __name__ == "__main__":
     app.run(debug=True)
