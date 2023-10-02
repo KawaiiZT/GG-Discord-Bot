e232e7754c273eafab08d2428fda4be2fd37967a
KawaiiZT <95923980+KawaiiZT@users.noreply.github.com>
2023-10-02 22:01:31 +0700

    update some progess
    dashboard website update

--- InviteDiscordwebsite/website.py
+++ InviteDiscordwebsite/website.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from oauth import Oauth
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html",discord_url= "https://github.com")
+    return render_template("index.html",discord_url= "Oauth.discord_login_url")
 
-
+@app.route("/login")
+def login():
+    code = request.args.get("code")
+    return "Success"
 if __name__ == "__main__":
     app.run(debug=True)
