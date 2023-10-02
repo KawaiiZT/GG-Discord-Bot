c4215473e9258342f7d0582f9a81c40a86e718a0
KawaiiZT <95923980+KawaiiZT@users.noreply.github.com>
2023-10-02 21:44:27 +0700

    making Dashboard site
    make some prototype website using Flask

---
+++ InviteDiscordwebsite/oauth.py
@@ -0,0 +1,10 @@
+import requests
+
+class Oauth:
+    client_id = ""
+    client_secret = ""
+    redirect_url = ""
+    scope = ""
+    discord_login_url = ""
+    discord_token_url = ""
+    discord_api_url = ""
