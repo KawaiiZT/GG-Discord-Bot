079f8d00a9e3dcd1687c4bb11d05f2a8bde6d7b1
KawaiiZT <95923980+KawaiiZT@users.noreply.github.com>
2023-10-12 19:12:37 +0700

    Update index.js

--- InviteDiscordwebsite/index.js
+++ InviteDiscordwebsite/index.js
@@ -9,6 +9,7 @@ app.use(express.static("public"));
 app.get("/",(req, res) =>{
     res.render('index', {bot: settings.website})
 })
+
 app.get("/commands", (req, res) => {
     res.render('commands', { bot: settings.website, commands: settings.commands });
 
