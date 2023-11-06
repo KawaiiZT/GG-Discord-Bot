cdd86dc31111cd119c2a62a0f3d5b259d06311e3
KawaiiZT <95923980+KawaiiZT@users.noreply.github.com>
2023-11-06 19:31:43 +0700

    Update welcome.py

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -1,19 +1,25 @@
 import discord
 import datetime
 from discord.ext import commands
+from discord import app_commands
 
-welcome_channel = #idของChannelในDiscord
+#idของChannelในDiscord
+welcome_channel = 1152256157604392982
 
 bot = commands.Bot(command_prefix='.', intents=discord.Intents.default())
 
-@bot.event
-async def on_member_join(member):
-    channel  = bot.get_channel(welcome_channel)
-    embed = discord.Embed(
-        description=f'ยินดีต้อนรับ **{member.mention}** เข้าสู่เซิพเวอร์.',
-        color=0xff55ff,
-        timestamp=datetime.datetime.now()
-    )
-    role = discord.utils.get(member.guild.roles, name='Member')
-    await member.add_roles(role)
-    await channel.send(embed=embed)
+class welcome(commands.Cog):
+    @bot.event
+    async def on_member_join(member):
+        channel  = bot.get_channel(welcome_channel)
+        embed = discord.Embed(
+            description=f'ยินดีต้อนรับ **{member.mention}** เข้าสู่เซิพเวอร์.',
+            color=0xff55ff,
+            timestamp=datetime.datetime.now()
+        )
+        role = discord.utils.get(member.guild.roles, name='Member')
+        await member.add_roles(role)
+        await channel.send(embed=embed)
+
+async def setup(client:commands.Bot) -> None:
+    await client.add_cog(welcome(client))
